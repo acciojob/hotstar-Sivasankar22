161fd157685f1c7dc88ee94a41204bdfa0990c3e
siva sankar <sivas2654@gmail.com>
2023-11-03 18:58:25 +0530

    frist

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -25,16 +26,30 @@ public class UserService {
     public Integer addUser(User user){
 
         //Jut simply add the user to the Db and return the userId returned by the repository
-        return null;
+        user=userRepository.save(user);
+        return user.getId();
     }
 
     public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
 
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
-
-
-        return null;
+        //Hint: Take out all the Webseries from the WebRepository
+        int age=0;
+        User user=userRepository.findById(userId).get();
+        if(user.getAge()<=18){
+            age=18;
+        }else{
+            age=Integer.MAX_VALUE;
+        }
+        Integer count=0;
+        List<WebSeries>webSeriesList=webSeriesRepository.findAll();
+        for (WebSeries webSeries:webSeriesList){
+            if(age>=webSeries.getAgeLimit()){
+                count++;
+            }
+        }
+        return count;
     }
 
 
